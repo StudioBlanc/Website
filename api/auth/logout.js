0ebcecce9bfe34acdd8bb7a6a04104fc9697c08c
StudioBlanc <Info@studioblanc.art>
2025-08-06 14:32:09 +0200

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,9 +0,0 @@
-export const config = { runtime: 'nodejs' };
-import { clearSession } from '../_session.js';
-
-export default async function handler(req, res) {
-  clearSession(res);
-  res.statusCode = 302;
-  res.setHeader('Location', '/login.html');
-  res.end();
-}
--- api/auth/me.js
+++
@@ -1,9 +0,0 @@
-export const config = { runtime: 'nodejs' };
-import { getRole } from '../_session.js';
-
-export default async function handler(req, res) {
-  const role = getRole(req);
-  if (!role) { res.statusCode = 401; return res.end('Unauthorized'); }
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ role }));
-}
